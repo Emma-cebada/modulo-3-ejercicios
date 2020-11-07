13c50b53e0497454502592b826cff7456b2b7780
Emma <emma.cebada@gmail.com>
2020-11-07 20:10:16 +0100

    Add exercise ItemList finished

--- leccion-4-ejercicio-1/my-template/src/components/ItemList.js
+++ leccion-4-ejercicio-1/my-template/src/components/ItemList.js
@@ -32,49 +32,25 @@ const arrayOfItems = [
   },
 ];
 
-// Bootstrap classes: badge, badge-info, text-muted
+let myArray = []
+let arrayPrices = []
+arrayPrices = arrayOfItems.filter((itemPrice) => itemPrice.price < 10)
+
+myArray = arrayPrices.map((itemInfo, i) => (<li key={i}>
+  <Item
+    name={itemInfo.name}
+    description={itemInfo.description} quantity={itemInfo.quantity}
+    category={itemInfo.category}
+    price={itemInfo.price}
+  />
+</li>
+));
 
 class ItemList extends React.Component {
   render() {
     return (
       <ul className="item-list">
-        <li>
-          <Item
-            name="Cereales con chocolate"
-            description="Cereales rellenos de chocolate"
-            quantity={2}
-            category="Cereales"
-            price={5}
-          />
-        </li>
-        <li>
-          <Item
-            name="Hamburguesa con queso"
-            description="Hamburguesa rica y saludable"
-            quantity={1}
-            category="Fast-food"
-            price={15}
-          />
-        </li>
-        <li>
-          <Item
-            name="Agua mineral"
-            description="Agua de un charco del Himalaya"
-            quantity={2}
-            category="Bebida"
-            price={5}
-          />
-        </li>
-        <li>
-          <Item
-            name="Helado de chocolate"
-            description="Fresquito y sabroso"
-            quantity={4}
-            category="Congelados"
-            price={6}
-
-          />
-        </li>
+        {myArray}
       </ul>
     );
   }
---
+++ leccion-5-ejercicio-4/my-template/src/components/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Destiny from './Destiny'
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Destiny></Destiny>
+      </div>
+    )
+  }
+}
+
+export default App;

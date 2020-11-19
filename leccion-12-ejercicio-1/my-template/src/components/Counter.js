018d3a5944b9a53634248b48d1cba9ea6b848f9f
Emma <emma.cebada@gmail.com>
2020-11-19 11:15:42 +0100

    Add exercise two with console.log

--- leccion-12-ejercicio-1/my-template/src/components/Counter.js
+++ leccion-12-ejercicio-1/my-template/src/components/Counter.js
@@ -9,21 +9,25 @@ class Counter extends React.Component {
     this.incrementCounter = this.incrementCounter.bind(this);
   }
   componentDidMount() {
+    console.log('Se está ejecuntando el método componentDidMount'); //Los console.log son del ejercicio 2
     // guardamos el identificador del interval para limpiarlo en componentWillUnmount
     this.intervalId = setInterval(this.incrementCounter, 1000);
     // NOTA: guardamos el identificador en un atributo de la clase y
     // no en el estado ya que no queremos pintar el identificador en el DOM
   }
   componentWillUnmount() {
+    console.log('Se está ejecuntando el método componentWillUnmount'); //Los console.log son del ejercicio 2
     // limpiamos el interval
     clearInterval(this.intervalId);
   }
   incrementCounter() {
+    console.log('Se está ejecuntando el método incrementCounter'); //Los console.log son del ejercicio 2
     this.setState(prevState => {
       return { counter: prevState.counter + 1 };
     });
   }
   render() {
+    console.log('Se está ejecuntando el método render'); //Los console.log son del ejercicio 2
     return <div>Contador: {this.state.counter}</div>;
   }
 }
